e3a4ef9ea95e30e0e8d7bf97ce4f25831c719779
valderyjr <valderyjunior2002@gmail.com>
2022-05-11 22:10:02 -0300

    criando rodape

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,13 @@
 import Header from 'components/Header'
 import ConteudoPrincipal from './Home/ConteudoPrincipal';
+import Footer from 'components/Footer'
 
 function App() {
   return (
     <>
 			<Header/>
 			<ConteudoPrincipal />
+			<Footer />
 		</>
   );
 }
